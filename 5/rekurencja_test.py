196f0ce1f081f5cb514daa7c5f5c09cbb23a4a43
Grzybolevsky <szymon436@gmail.com>
2019-11-13 08:55:56 +0100

    Zestaw 5

---
+++ 5/rekurencja_test.py
@@ -0,0 +1,13 @@
+import rekurencja
+import rekurencja as rek
+from rekurencja import *
+from rekurencja import factorial
+from rekurencja import fibonacci as fib
+
+print rekurencja.factorial(6)
+print rekurencja.fibonacci(5)
+print rek.factorial(6)
+print rek.fibonacci(5)
+print factorial(6)
+print fibonacci(5)
+print fib(5)
