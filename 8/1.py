1e54cac8e863d4151fe5e211da23595014d15364
Grzybolevsky <szymon436@gmail.com>
2019-12-04 10:10:34 +0100

    fixup! Zestaw 8

--- 8/1.py
+++ 8/1.py
@@ -3,8 +3,10 @@ def solve1(a, b, c):
     """Rozwiązywanie równania liniowego a x + b y + c = 0."""
     if b == 0:
         solution = "x = {0}".format(float(-c) / a)
-    elif a == 0:
+    elif a == 0 and c != 0:
         raise ValueError("Equation hasn't solve!")
+    elif c == 0:
+        solution = "0 = 0 (równanie jest prawdziwe dla dowolnych x, y)"
     else:
         solution = "y = {0}*x + ({1})".format(-float(a) / float(b), -float(c) / float(b))
     return solution
